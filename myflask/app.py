f1a78e157b5755a621798a49aa8454c1350c9afe
Kofi Darkoh <kofidarkoh@yandex.com>
2020-05-30 22:44:42 +0000

    updated

--- myflask/app.py
+++
@@ -1,5 +0,0 @@
-from blog import app
-#http_server = WSGIServer(('127.0.0.1', 5000), app)
-# http_server.serve_forever()
-
-app.run(debug = True)
--- myflask/blog/static/jodit/src/plugins/clipboard/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./cut";
-export * from "./paste";
-export * from "./paste-storage";
-import "./copyformat";
